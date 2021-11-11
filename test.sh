824d79a39294e60d989f996f31dc6807f3da0029
takashiidobe <idobetakashi@gmail.com>
2021-11-11 08:48:29 -0600

    add tests

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+assert() {
+  expected="$1"
+
+	actual=$(exec	./hello)
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$expected => $actual"
+  else
+    echo "$expected => $actual, but got $actual"
+    exit 1
+  fi
+}
+
+assert "Hello world\n"
+
+echo "OK"
